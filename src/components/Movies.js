5ce0c1a29c9f3d2ab17fc8a5c0e505aea4d230a6
Beomseok Choi <cbs412@naver.com>
2021-05-29 21:24:21 +0900

    we got some format of the ticket this is white version

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,6 +1,6 @@
 const movies = [{
     resultCode : "000000",
-    title:"이웃집토토로"
+    title:"이웃집 토토로"
 }, {
     resultCode : "000001",
     title:"이웃집도로로"
--- src/components/Test.js
+++ src/components/Test.js
@@ -24,9 +24,36 @@ function Test({type, movie, image, title, choices, onClick}){
     }
     else{
         return (
-            <div>
-                <h1>당신의 영화는,</h1>
-                <h2>{movie.title}</h2>
+            <div className="result_container">
+                <div className="moviePoster">
+                    <img src="" />
+                </div>
+                <div className="movie">
+                    <div className ="movieTitle">
+                        <span>오늘 당신의 영화는,</span>
+                        <span>"{movie.title}"</span>
+                        <span>(2001, 미야자키 하야오)</span>
+                    </div>
+                    <div className ="movieDescription">
+                        <div>일상에 너무 지쳐 언젠가부터 동심을 잃어버린 당신. 토로로의 털속에 파묻혀 따뜻해져 보는건 어떨까.</div>
+                    </div>  
+                    <div className="ticketInfo">
+                        <div className="time">
+                            19:30
+                        </div>
+                        <div className="theater">
+                            2관
+                        </div>
+                        <div className="seat">
+                            H열 15번
+                        </div>
+                    </div>   
+                    <div className="payment">
+                        <span> 9,000 노마드코인</span>
+                        <span>(현장결제)</span>
+                    </div>
+                </div>
+                
             </div>
             
         )
